9009f373b7a5a3f27b0cfb3488eba9e5b80aa135
liumochi <m18846089770@163.com>
2017-05-21 22:14:19 +0800

    vue
    基于vue的购物系统

---
+++ eshop/src/components/CommonContent.vue
@@ -0,0 +1,54 @@
+<template>
+	 <div id="categoires" class="wrapper">
+        <a href="#">
+            <div class="fashion-a sections">
+                <h4>Fashion</h4>
+                <p>dignissim</p>
+            </div>
+        </a>
+        <a href="#">
+            <div class="fashion-b sections">
+                <h4>Beauty</h4>
+                <p>fermentum</p>
+            </div>
+        </a>
+        <a href="#">
+            <div class="fashion-c sections">
+                <h4>Creativity</h4>
+                <p>vulputate</p>
+            </div>
+        </a>
+    </div>
+    <div id="product" class="wrapper">
+        <ul id="product-tags">
+            <li>Best seller</li>
+            <li>Popular</li>
+            <li>New Arrivals</li>
+        </ul>
+        <div id="loading">
+            <img src="img/loading.gif" alt="">
+        </div>
+        <ul id="product-list">
+        
+        </ul>
+        <div id="load-more">
+            加载更多商品...
+        </div>
+    </div>
+    <div class="fotter-logo">
+        <div class="container">
+            <div class="ftr-logo"><h3><a href="index.html">NEW FASHIONS</a></h3></div>
+            <div class="ftr-info">
+                <p>Copyright &copy; 2015.Company name All rights reserved. <a href="http://www.cssmoban.com/" target="_blank" title=""></a> - Collect from <a href="" title="" target="_blank">网上商城</a></p>
+            </div>	 <div class="clearfix"></div>
+        </div>
+    </div>
+</template>
+<script>
+	
+</script>
+<style>
+	@import '../assets/css/bootstrap.css';
+  	@import '../assets/css/index.css';
+  	@import '../assets/css/style.css';
+</style>
---
+++ eshop/src/components/CommonHeader.vue
@@ -0,0 +1,232 @@
+<template>
+<div id="header">
+    <div class="top-menu wrapper">
+        <a id="login">LOGIN</a>
+        <div id="cart-box">
+            <a href="product/cart">
+                Cart: <span id="money">
+                  
+                </span>(<span id="quantity">
+                    
+                </span>)
+            </a>
+            <p id="empty">empty cart</p>
+        </div>
+        <h1 id="logo">NEW FASHIONS</h1>
+    </div>
+    <ul id="nav" class="wrapper">
+        <li class="main-menu"><a href="javascript:;" class="active" data-cate="0">首页</a></li>
+        <li class="women main-menu">
+            <a href="javascript:;" data-cate="2">女士</a>
+            <div class="sub-menu">
+                <div class="col">
+                    <h4>SHOP</h4>
+                    <ul>
+                        <li><a href="">new arrivals</a></li>
+                        <li><a href="products.php">men</a></li>
+                        <li><a href="../../products.html">women</a></li>
+                        <li><a href="../../products.html">accessories</a></li>
+                        <li><a href="../../products.html">kids</a></li>
+                        <li><a href="../../products.html">brands</a></li>
+
+                    </ul>
+                </div>
+                <div class="col">
+                    <h4>HELP</h4>
+                    <ul>
+                        <li><a href="../../products.html">trends</a></li>
+                        <li><a href="../../products.html">sale</a></li>
+                        <li><a href="../../products.html">style videos</a></li>
+                        <li><a href="../../products.html">accessories</a></li>
+                        <li><a href="../../products.html">kids</a></li>
+                        <li><a href="../../products.html">style videos</a></li>
+
+                    </ul>
+                </div>
+                <div class="col">
+                    <h4>PRODUCTS</h4>
+                    <ul>
+                        <li><a href="../../products.html">trends</a></li>
+                        <li><a href="../../products.html">sale</a></li>
+                        <li><a href="../../products.html">style videos</a></li>
+                        <li><a href="../../products.html">accessories</a></li>
+                        <li><a href="../../products.html">kids</a></li>
+                        <li><a href="../../products.html">style videos</a></li>
+
+                    </ul>
+                </div>
+                <div class="col">
+                    <h4>MY COMPANY</h4>
+                    <ul>
+                        <li><a href="../../products.html">trends</a></li>
+                        <li><a href="../../products.html">sale</a></li>
+                        <li><a href="../../products.html">style videos</a></li>
+                        <li><a href="../../products.html">accessories</a></li>
+                        <li><a href="../../products.html">kids</a></li>
+                        <li><a href="../../products.html">style videos</a></li>
+
+                    </ul>
+                </div>
+                <div class="col">
+                    <h4>POPULAR</h4>
+                    <ul>
+                        <li><a href="../../products.html">new arrivals</a></li>
+                        <li><a href="../../products.html">men</a></li>
+                        <li><a href="../../products.html">women</a></li>
+                        <li><a href="../../products.html">accessories</a></li>
+                        <li><a href="../../products.html">kids</a></li>
+                        <li><a href="../../products.html">style videos</a></li>
+
+                    </ul>
+                </div>
+            </div>
+        </li>
+        <li class="men main-menu">
+            <a href="javascript:;" data-cate="1">男士</a>
+            <div class="sub-menu">
+                <div class="col">
+                    <h4>SHOP</h4>
+                    <ul>
+                        <li><a href="../../men.html">new arrivals</a></li>
+                        <li><a href="../../men.html">men</a></li>
+                        <li><a href="../../men.html">women</a></li>
+                        <li><a href="../../men.html">accessories</a></li>
+                        <li><a href="../../men.html">kids</a></li>
+                        <li><a href="../../men.html">brands</a></li>
+
+
+                    </ul>
+                </div>
+                <div class="col">
+                    <h4>HELP</h4>
+                    <ul>
+                        <li><a href="../../men.html">trends</a></li>
+                        <li><a href="../../men.html">sale</a></li>
+                        <li><a href="../../men.html">style videos</a></li>
+                        <li><a href="../../men.html">accessories</a></li>
+                        <li><a href="../../men.html">kids</a></li>
+                        <li><a href="../../men.html">style videos</a></li>
+
+                    </ul>
+                </div>
+                <div class="col">
+                    <h4>PRODUCTS</h4>
+                    <ul>
+                        <li><a href="../../men.html">trends</a></li>
+                        <li><a href="../../men.html">sale</a></li>
+                        <li><a href="../../men.html">style videos</a></li>
+                        <li><a href="../../men.html">accessories</a></li>
+                        <li><a href="../../men.html">kids</a></li>
+                        <li><a href="../../men.html">style videos</a></li>
+
+
+                    </ul>
+                </div>
+                <div class="col">
+                    <h4>MY COMPANY</h4>
+                    <ul>
+                        <li><a href="../../men.html">trends</a></li>
+                        <li><a href="../../men.html">sale</a></li>
+                        <li><a href="../../men.html">style videos</a></li>
+                        <li><a href="../../men.html">accessories</a></li>
+                        <li><a href="../../men.html">kids</a></li>
+                        <li><a href="../../men.html">style videos</a></li>
+
+                    </ul>
+                </div>
+                <div class="col">
+                    <h4>POPULAR</h4>
+                    <ul>
+
+                        <li><a href="../../men.html">new arrivals</a></li>
+                        <li><a href="../../men.html">men</a></li>
+                        <li><a href="../../men.html">women</a></li>
+                        <li><a href="../../men.html">accessories</a></li>
+                        <li><a href="../../men.html">kids</a></li>
+                        <li><a href="../../men.html">style videos</a></li>
+
+                    </ul>
+                </div>
+            </div>
+        </li>
+        <li class="main-menu"><a href="javascript:;" data-tag="1">潮人推荐</a></li>
+        <li class="main-menu"><a href="javascript:;">博客</a></li>
+        <li class="main-menu">
+            <a href="javascript:;">关于我们</a>
+            <div class="sub-menu">
+                <div class="col">
+                    <h4>SHOP</h4>
+                    <ul>
+                        <li><a href="../../shop.html">new arrivals</a></li>
+                        <li><a href="../../shop.html">men</a></li>
+                        <li><a href="../../shop.html">women</a></li>
+                        <li><a href="../../shop.html">accessories</a></li>
+                        <li><a href="../../shop.html">kids</a></li>
+                        <li><a href="../../shop.html">brands</a></li>
+
+                    </ul>
+                </div>
+                <div class="col">
+                    <h4>HELP</h4>
+                    <ul>
+                        <li><a href="../../shop.html">trends</a></li>
+                        <li><a href="../../shop.html">sale</a></li>
+                        <li><a href="../../shop.html">style videos</a></li>
+                        <li><a href="../../shop.html">accessories</a></li>
+                        <li><a href="../../shop.html">kids</a></li>
+                        <li><a href="../../shop.html">style videos</a></li>
+
+                    </ul>
+                </div>
+                <div class="col">
+                    <h4>PRODUCTS</h4>
+                    <ul>
+                        <li><a href="../../shop.html">trends</a></li>
+                        <li><a href="../../shop.html">sale</a></li>
+                        <li><a href="../../shop.html">style videos</a></li>
+                        <li><a href="../../shop.html">accessories</a></li>
+                        <li><a href="../../shop.html">kids</a></li>
+                        <li><a href="../../shop.html">style videos</a></li>
+
+                    </ul>
+                </div>
+                <div class="col">
+                    <h4>MY COMPANY</h4>
+                    <ul>
+                        <li><a href="../../shop.html">trends</a></li>
+                        <li><a href="../../shop.html">sale</a></li>
+                        <li><a href="../../shop.html">style videos</a></li>
+                        <li><a href="../../shop.html">accessories</a></li>
+                        <li><a href="../../shop.html">kids</a></li>
+                        <li><a href="../../shop.html">style videos</a></li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <h4>POPULAR</h4>
+                    <ul>
+                        <li><a href="../../shop.html">new arrivals</a></li>
+                        <li><a href="../../shop.html">men</a></li>
+                        <li><a href="../../shop.html">women</a></li>
+                        <li><a href="../../shop.html">accessories</a></li>
+                        <li><a href="../../shop.html">kids</a></li>
+                        <li><a href="../../shop.html">style videos</a></li>
+                    </ul>
+                </div>
+            </div>
+        </li>
+    </ul>
+    <div class="caption">
+        <h1>FASHION AND CREATIVITY</h1>
+        <p>Sed dapibus est a lorem dictum, id dignissim lacus fermentum. Nulla ut nibh in libero maximus pretium
+            Nunc vulputate vel tellus ac elementum. Duis nec tincidunt dolor, ac dictum eros.</p>
+    </div>
+</div>
+</template>
+<script>
+	
+</script>
+<style>
+ 	@import '../assets/css/bootstrap.css';
+  	@import '../assets/css/index.css';
+  	@import '../assets/css/style.css';
+</style>
